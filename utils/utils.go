d7c655706a2df62326b1d4de30c69d522a35e4f8
qing <qingcc0503@163.com>
2019-12-05 16:31:09 +0800

    add

--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,21 @@
 package utils
 
-import "unsafe"
+import (
+	"log"
+	"runtime/debug"
+	"unsafe"
+)
 
 func Bytes2String(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
+}
+
+func RecoverPanic(isPrintStack bool)  {
+	if r := recover(); r != nil {
+		log.Println(r)
+		if isPrintStack {
+			debug.PrintStack()
+		}
+	}
+	return
 }
